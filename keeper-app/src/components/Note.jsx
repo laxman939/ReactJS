9ff3a87a3d44c8ee8522d4f4db3fd2f483e36b35
laxman <ramlaxman947@gmail.com>
2021-11-01 11:08:02 +0530

    added boilerplate code and styles

---
+++ keeper-app/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>For Title</h1>
+      <p>For Content</p>
+    </div>
+  );
+}
+
+export default Note;
