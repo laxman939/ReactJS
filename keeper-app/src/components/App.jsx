b55bd7b51b0adb0a92819e6129c2b0ef28c37a2e
laxman <ramlaxman947@gmail.com>
2021-11-05 00:29:59 +0530

    added existing notes using map and arrow function

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
+import notes from "../notes";
+
+//console.log(notes)-->Array of objects
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((singleNote) => (
+        <Note title={singleNote.title} cont={singleNote.content} />
+      ))}
       <Footer />
     </div>
   );
---
+++ keeper-app/src/components/CreateNote.js
@@ -0,0 +1,9 @@
+// import React from "react";
+// import Note from "./Note";
+
+// function CreateNote(singleNote) {
+//   return <Note title={singleNote.title} cont={singleNote.content} />;
+// }
+
+// export default CreateNote;
+/*Used directly in App.js using Arrow function */
