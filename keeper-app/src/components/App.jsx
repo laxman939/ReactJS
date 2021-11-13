e2fad9534d3525babee72aa2d78bfeebe7186911
laxman <ramlaxman947@gmail.com>
2021-11-14 01:21:09 +0530

    implemented addNotes functionality

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import CreateNote from "./CreateNote";
 import Footer from "./Footer";
 import Header from "./Header";
-import Note from "./Note";
-import notes from "../notes";
+import ShowNote from "./ShowNote";
 
 //console.log(notes)-->Array of objects
 
 function App() {
+  //To store data into an array
+  const [showNotes, setShowNotes] = useState([]);
+
+  //function to store recieved data from CreateNote into an array, when we click on add button
+  function addNote(notes) {
+    //console.log(notes)
+    setShowNotes((prevNotes) => {
+      return [...prevNotes, notes]; //To display previousnotes
+    });
+  }
   return (
     <div>
       <Header />
-      {notes.map((singleNote) => (
-        <Note title={singleNote.title} cont={singleNote.content} />
-      ))}
+      <CreateNote onAdd={addNote} />
+      <ShowNote />
       <Footer />
     </div>
   );
--- keeper-app/src/components/CreateNote.js
+++ keeper-app/src/components/CreateNote.js
@@ -1,9 +1,52 @@
-// import React from "react";
-// import Note from "./Note";
+import React, { useState } from "react";
 
-// function CreateNote(singleNote) {
-//   return <Note title={singleNote.title} cont={singleNote.content} />;
-// }
+function CreateNote(props) {
+  const [notes, setNotes] = useState({
+    title: "",
+    content: "",
+  });
 
-// export default CreateNote;
+  function handleChange(event) {
+    const { name, value } = event.target;
+    //console.log(name, value);name--title/content; value--user entered text in title/content
+
+    setNotes((prevNotes) => ({ ...prevNotes, [name]: value }));
+  }
+
+  //send data return to app.js using recieved function(onAdd)
+  function handleSubmit(event) {
+    props.onAdd(notes); //passed notes to app.js
+
+    //To make text field empty
+    setNotes({
+      title: "",
+      content: "",
+    });
+
+    event.preventDefault(); //To prevent default browsing/refreshing of form
+  } //can use as arrow function directly for onSubmit event
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="title"
+          placeholder="Title"
+          onChange={handleChange}
+          value={notes.title}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a note.."
+          onChange={handleChange}
+          rows="3"
+          value={notes.content}
+        />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateNote;
 /*Used directly in App.js using Arrow function */
