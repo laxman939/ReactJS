35c1c0741dcc182aa1de16654fd278407b8b97e5
laxman <ramlaxman947@gmail.com>
2021-11-23 18:49:23 +0530

    Intro of Component lifecycle methods

---
+++ reactjs-v-yt/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Concepts from "./Components-lifeCycle-methods/Concepts";
+
+function App() {
+  return (
+    <div className="App">
+      <ol>
+        <Concepts />
+      </ol>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactjs-v-yt/src/Components-lifeCycle-methods/Concepts.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Concepts() {
+  return (
+    <div>
+      <h1>Component Life-Cycle Methods</h1>
+      <div>
+        <h2>
+          1.Mounting -- <small>Putting Elements into the DOM</small>
+        </h2>
+        <h2>
+          2.Updating -- <small>When component is updated</small>
+        </h2>
+        <h2>
+          2.Unmounting -- <small>When component is removed from DOM</small>
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default Concepts;
