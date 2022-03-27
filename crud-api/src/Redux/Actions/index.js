b64f3fde166723bf8ca61cda723ed7ff120820dc
laxman <ramlaxman947@gmail.com>
2022-03-27 11:32:48 +0530

    Perform Update operation

--- crud-api/src/Redux/Actions/index.js
+++ crud-api/src/Redux/Actions/index.js
@@ -15,12 +15,21 @@ const userAdd = () => ({
   type: types.ADD_USER,
 });
 
+const userUpdate = () => ({
+  type: types.UPDATE_USER,
+});
+
+const getUser = (user) => ({
+  type: types.GET_SINGLE_USER,
+  payload: user,
+});
+
 export const loadUsers = () => {
   return function (dispatch) {
     axios
       .get(`${process.env.REACT_APP_API}`)
       .then((resp) => {
-        console.log("resp " + resp.data);
+        // console.log("resp " + resp.data);
         dispatch(getUsers(resp.data));
       })
       .catch((error) => console.log("Error " + error));
@@ -32,7 +41,7 @@ export const deleteUser = (id) => {
     axios
       .delete(`${process.env.REACT_APP_API}/${id}`)
       .then((resp) => {
-        console.log("resp " + resp.data);
+        // console.log("resp " + resp.data);
         dispatch(userDelete());
         dispatch(loadUsers());
       })
@@ -45,9 +54,33 @@ export const addUser = (user) => {
     axios
       .post(`${process.env.REACT_APP_API}`, user)
       .then((resp) => {
-        console.log("resp added" + resp.data);
+        // console.log("resp added" + resp.data);
         dispatch(userAdd());
         dispatch(loadUsers());
+      })
+      .catch((error) => console.log("Error " + error));
+  };
+};
+
+export const getSingleUser = (id) => {
+  return function (dispatch) {
+    axios
+      .get(`${process.env.REACT_APP_API}/${id}`)
+      .then((resp) => {
+        // console.log("resp " + resp.data);
+        dispatch(getUser(resp.data));
+      })
+      .catch((error) => console.log("Error " + error));
+  };
+};
+
+export const updateUser = (user, id) => {
+  return function (dispatch) {
+    axios
+      .put(`${process.env.REACT_APP_API}/${id}`, user)
+      .then((resp) => {
+        // console.log("resp " + resp.data);
+        dispatch(userUpdate());
       })
       .catch((error) => console.log("Error " + error));
   };
