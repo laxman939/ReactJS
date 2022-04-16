01958b694b072fbdd02cb1503d38d40e01056dc6
laxman <ramlaxman947@gmail.com>
2022-04-16 17:52:47 +0530

    Added Routes

--- useform-hook/src/App.tsx
+++ useform-hook/src/App.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 import './App.css';
+import Header from './Components/Header';
+import Result from './Pages/Result';
+import Step1 from './Pages/Step1';
+import Step2 from './Pages/Step2';
+import Step3 from './Pages/Step3';
 // import SampleForm from './SampleForm/SampleForm';
 
 function App() {
 return (
   <>
   {/* <SampleForm/> */}
-
+<Header/>
 <Router>
   <Routes>
-    <Route path='/'/>
+    <Route path='/' element={<Step1/>}/>
+    <Route path='/step2' element={<Step2/>}/>
+    <Route path='/step3' element={<Step3/>}/>
+    <Route path='/result' element={<Result/>}/>
   </Routes>
 </Router>
   </>
