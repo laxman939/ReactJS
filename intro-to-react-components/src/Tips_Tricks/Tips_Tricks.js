ed0fa9dc2054ca61c13b29503dfb444f7dacbb38
laxman <ramlaxman947@gmail.com>
2024-11-05 16:31:18 +0530

    Added some useful code snippets

--- intro-to-react-components/src/Tips_Tricks/Tips_Tricks.js
+++ intro-to-react-components/src/Tips_Tricks/Tips_Tricks.js
@@ -1,7 +1,7 @@
 // React Tips & Tricks for Beginers to Experts
 // --> KISS - Keep It Simple Stupid - Less code less bugs
 
-import { useCallback, useMemo } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 // 1. When setting default values for props, do it while destructuring them
 function Button({
@@ -105,7 +105,6 @@ function UserPosts({ user }) {
 
 // 8. Use functions (inline or not) to avoid polluting your scope with intermediate variables
 // The variables gradeSum and gradeCountare scoped within computeAverageGrade function.
-
 function Grade({ grades }) {
   if (grades.length === 0) {
     return <>No grades available.</>;
@@ -136,4 +135,156 @@ function Grade({ grades }) {
   return <>Average Grade: {computeAverageGrade()}</>;
 }
 
-// Leverage the children props for cleaner code (and performance benefits)
+//9. Leverage the children props for cleaner code (and performance benefits)
+
+//10. We focus on the input as soon as it is available.
+function InputComponent() {
+  const ref = useCallback((inputNode) => {
+    inputNode?.focus();
+  }, []);
+
+  return <input ref={ref} type="text" />;
+}
+
+// 11. Prefer named exports over default exports
+/// Default export
+// import Dashboard from "/path/to/Dashboard"
+
+/// Named export
+// import { Dashboard1 } from "/path/to/Dashboard"
+
+// 12. Never create a state for a value that can be derived from other state or props
+// More state = more trouble --- Every piece of state can trigger a re-render and make resetting the state a hassle.
+
+// 13. Keep the state at the lowest level necessary to minimize re-renders
+// Whenever the state changes inside a component, React re-renders the component and all its children (there is an exception with children wrapped in memo).
+// This happens even if those children don't use the changed state. To minimize re-renders, move the state down the component tree as far as possible.
+// BAD --  When sortOrder changes, both LeftSidebar and RightSidebar re-render.
+function AppOne() {
+  const [sortOrder, setSortOrder] = useState("popular");
+  return (
+    <div className="App">
+      {/* <LeftSidebar /> */}
+      <MainOne sortOrder={sortOrder} setSortOrder={setSortOrder} />
+      {/* <RightSidebar /> */}
+    </div>
+  );
+}
+
+function MainOne({ sortOrder, setSortOrder }) {
+  return (
+    <div>
+      <Button
+        onClick={() => setSortOrder("popular")}
+        active={sortOrder === "popular"}
+      >
+        Popular
+      </Button>
+      <Button
+        onClick={() => setSortOrder("latest")}
+        active={sortOrder === "latest"}
+      >
+        Latest
+      </Button>
+    </div>
+  );
+}
+
+// GOOD -- sortOrder change will only affect Main.
+function AppTwo() {
+  return (
+    <div className="App">
+      {/* <LeftSidebar /> */}
+      <MainTwo />
+      {/* <RightSidebar /> */}
+    </div>
+  );
+}
+
+function MainTwo() {
+  const [sortOrder, setSortOrder] = useState("popular");
+  return (
+    <div>
+      <Button
+        onClick={() => setSortOrder("popular")}
+        active={sortOrder === "popular"}
+      >
+        Popular
+      </Button>
+      <Button
+        onClick={() => setSortOrder("latest")}
+        active={sortOrder === "latest"}
+      >
+        Latest
+      </Button>
+    </div>
+  );
+}
+
+// 14. Clarify the distinction between the initial state and the current state
+// Good: Naming is clear about what is the initial state and what is current.
+function Main({ initialSortOrder }) {
+  const [sortOrder, setSortOrder] = useState(initialSortOrder);
+  return (
+    <div>
+      <Button
+        onClick={() => setSortOrder("popular")}
+        active={sortOrder === "popular"}
+      >
+        Popular
+      </Button>
+      <Button
+        onClick={() => setSortOrder("latest")}
+        active={sortOrder === "latest"}
+      >
+        Latest
+      </Button>
+    </div>
+  );
+}
+
+// 15. Update the state based on the previous state, especially when memoizing with useCallback
+// BAD -- handleAddTodo and handleRemoveTodo change whenever todos changes.
+function UpdateOne() {
+  const [todos, setToDos] = useState([]);
+  const handleAddTodo = useCallback(
+    (todo) => {
+      setToDos([...todos, todo]);
+    },
+    [todos]
+  );
+
+  const handleRemoveTodo = useCallback(
+    (id) => {
+      setToDos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos]
+  );
+
+  return (
+    <div className="App">
+      {/* <TodoInput onAddTodo={handleAddTodo} />
+      <TodoList todos={todos} onRemoveTodo={handleRemoveTodo} /> */}
+    </div>
+  );
+}
+
+// GOOD -- handleAddTodo and handleRemoveTodo change whenever todos changes.
+function UpdateTwo() {
+  const [todos, setToDos] = useState([]);
+
+  const handleAddTodo = useCallback((todo) => {
+    setToDos((prevTodos) => [...prevTodos, todo]);
+  }, []);
+
+  const handleRemoveTodo = useCallback((id) => {
+    setToDos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  }, []);
+
+  return (
+    <div className="App">
+      {/* <TodoInput onAddTodo={handleAddTodo} />
+      <TodoList todos={todos} onRemoveTodo={handleRemoveTodo} /> */}
+    </div>
+  );
+}
