0441e6c465d68491ff98e98579ea95aa5e639410
laxman <ramlaxman947@gmail.com>
2025-01-21 07:23:10 +0530

    Sample React Fiber example

--- threeJS/threejs_sample/src/App.jsx
+++ threeJS/threejs_sample/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Canvas>
+        <mesh position-x={-2} rotation-x={Math.PI * 0.5}>
+          <torusKnotGeometry />
+          <meshNormalMaterial />
+        </mesh>
+        <mesh position={[2, 0, 0]} scale={1.5}>
+          <torusKnotGeometry />
+          <meshNormalMaterial />
+        </mesh>
+      </Canvas>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
