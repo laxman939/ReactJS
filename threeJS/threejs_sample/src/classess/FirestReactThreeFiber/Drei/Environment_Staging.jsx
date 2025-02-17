bb0b0f3f9ea5a6118c1b8608e0ba2ab7fc307bd7
laxman <ramlaxman947@gmail.com>
2025-02-17 07:44:57 +0530

    Sparkles helper

--- threeJS/threejs_sample/src/classess/FirestReactThreeFiber/Drei/Environment_Staging.jsx
+++ threeJS/threejs_sample/src/classess/FirestReactThreeFiber/Drei/Environment_Staging.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unknown-property */
-import { OrbitControls, useHelper } from "@react-three/drei";
+import { OrbitControls, Sparkles, useHelper } from "@react-three/drei";
 import { useRef } from "react";
 import * as THREE from "three";
 
@@ -7,7 +7,7 @@ import * as THREE from "three";
 
 const Environment_Staging = () => {
   const lightRef = useRef();
-  useHelper(lightRef, THREE.DirectionalLightHelper, 1);
+  //   useHelper(lightRef, THREE.DirectionalLightHelper, 1);
 
   return (
     <>
@@ -16,13 +16,21 @@ const Environment_Staging = () => {
       <directionalLight ref={lightRef} castShadow />
       {/* <directionalLight position={[0, 1, 0]} color="white" intensity={4} /> */}
 
-      <mesh castShadow>
+      {/* <mesh castShadow>
         <boxGeometry />
         <meshStandardMaterial color="#C7CAC7" />
-      </mesh>
+      </mesh> */}
+      <Sparkles
+        count={400}
+        speed={2}
+        opacity={3}
+        color="#68c2ED"
+        scale={[10, 10, 10]}
+        size={2}
+      />
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} receiveShadow>
-        <planeGeometry args={[8, 8]} />
+        <planeGeometry args={[2, 2]} />
         <meshStandardMaterial color="#CC3941" side={THREE.DoubleSide} />
       </mesh>
     </>
