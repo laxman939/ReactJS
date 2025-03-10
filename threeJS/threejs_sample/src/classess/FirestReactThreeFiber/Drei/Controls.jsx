17e904a1604227e4abefeb126d8418b1ba49a619
laxman <ramlaxman947@gmail.com>
2025-03-10 21:25:38 +0530

    PresentationControls helper from drei

--- threeJS/threejs_sample/src/classess/FirestReactThreeFiber/Drei/Controls.jsx
+++ threeJS/threejs_sample/src/classess/FirestReactThreeFiber/Drei/Controls.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-unknown-property */
 import CameraControl from "./Controls/CameraControl";
 import OrbitControl from "./Controls/OrbitControl";
+import PresentationControl from "./Controls/PresentationControl";
 
 // Helpers to know the direction of light
 
@@ -8,7 +9,8 @@ const Controls = () => {
   return (
     <>
       {/* <CameraControl /> */}
-      <OrbitControl />
+      {/* <OrbitControl /> */}
+      <PresentationControl />
     </>
   );
 };
